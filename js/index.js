e7f882059299c3b2628bee6dfd694d856d9dfca0
serveriator <97800968+serveriator@users.noreply.github.com>
2023-04-14 02:43:01 +0330

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,25 @@
+/*var changer = 26;
+console.log(changer);
+
+changer = 22;
+console.log(changer);
+
+const pi = 7888;
+console.log(pi);
+
+
+sar = 10;
+ser = 20;
+sir = 5;
+eq = sar-ser+5;
+console.log(eq);
+
+var tf = ('ser' == 'ser');
+console.log(tf)
+
+var type = '';
+console.log(typeof type)
+*/
+one = 1;
+twenty = 20;
+console.log(one)
